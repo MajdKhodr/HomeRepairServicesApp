260ca4e5f05af008a0fe3f4dcfbc986643dc3bec
AliKhanafer7 <ali.h.khanafer@gmail.com>
2018-10-06 14:47:39 -0400

    Edited sign up page and added string ressources

---
+++ app/src/main/java/com/scum/seg/ondemandhomerepairservices/SignUpPageActivity.java
@@ -0,0 +1,13 @@
+package com.scum.seg.ondemandhomerepairservices;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUpPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_page);
+    }
+}
