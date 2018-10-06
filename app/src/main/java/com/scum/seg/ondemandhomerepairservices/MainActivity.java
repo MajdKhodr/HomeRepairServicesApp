7edb908053f6c52ca804cb8c0084554c508e9e0c
Alexi10 <alexisharara@gmail.com>
2018-10-06 17:54:13 -0400

    fixed stuff

--- app/src/main/java/com/scum/seg/ondemandhomerepairservices/MainActivity.java
+++ app/src/main/java/com/scum/seg/ondemandhomerepairservices/MainActivity.java
@@ -1,5 +1,6 @@
 package com.scum.seg.ondemandhomerepairservices;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,6 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Temp(){
+        Intent intent = new Intent(this, SignUpPageActivity.class);
+        startActivity(intent);
     }
 
 }
