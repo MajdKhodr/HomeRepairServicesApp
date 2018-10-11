3529ee0be67ff623b5ff541a880770ddc31c1446
Alexi <alexisharara@gmail.com>
2018-10-11 10:20:24 -0400

    sign in now works

--- app/src/main/java/com/scum/seg/ondemandhomerepairservices/MainActivity.java
+++ app/src/main/java/com/scum/seg/ondemandhomerepairservices/MainActivity.java
@@ -6,7 +6,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
+// Required for firebase
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -25,17 +27,36 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void signInUser(View view){
+    public void signInUser(View view) {
+        // Get editText reference
+        final EditText mUsername = findViewById(R.id.username_signin_edittext);
+        final EditText mPassword = findViewById(R.id.password_signin_edittext);
+
+        // Get database reference
         DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference("Users");
 
+        // Add on data change listener to database to fetch data
         ValueEventListener userListener = new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 try {
+                    boolean found = false;
+                    // Loop through list of users checking if the given username and password match an account
                     for (DataSnapshot childSnapshot : dataSnapshot.getChildren()) {
                         User user = childSnapshot.getValue(User.class);
+                        if (user.getUserName().equals(mUsername.getText().toString().trim())
+                                && AESCrypt.decrypt(user.getPassword()).equals(mPassword.getText().toString().trim())) {
+                            found = true;
+                            break;
+                        }
                     }
-                } catch (Exception e){
+                    if (!found) {
+                        Log.d(TAG, "Sign in unsuccessful");
+                    }else{
+                        Log.d(TAG, "Sign in successful");
+                    }
+
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
@@ -48,7 +69,8 @@ public class MainActivity extends AppCompatActivity {
         mDatabase.addValueEventListener(userListener);
     }
 
-    public void signUpUser(View view){
+    // Sends user to sign up page
+    public void signUpUser(View view) {
         Intent intent = new Intent(this, SignUpPageActivity.class);
         startActivity(intent);
 
