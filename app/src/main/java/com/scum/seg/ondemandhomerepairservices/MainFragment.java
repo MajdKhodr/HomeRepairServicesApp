f13d7867335f96f130065470ff281c1310fd2e9e
GeorgesGITHUB <georgesatallahbusiness@gmail.com>
2018-10-20 13:42:01 -0400

    Added the welcome message to the home screen fragment

--- app/src/main/java/com/scum/seg/ondemandhomerepairservices/MainFragment.java
+++ app/src/main/java/com/scum/seg/ondemandhomerepairservices/MainFragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class MainFragment extends Fragment {
     @Override
@@ -12,10 +13,28 @@ public class MainFragment extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
+    //Instance variables
+    TextView mWelcomeTextView;
+    String mWelcomeString;
+    User user;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View v = inflater.inflate(R.layout.fragment_main, container, false);
+
+        //Get's user information from Sign in Activity
+        user = (User) getActivity().getIntent().getSerializableExtra("User");
+
+        mWelcomeTextView = (TextView) v.findViewById(R.id.welcomeMSG_textView);
+
+        mWelcomeString = "Welcome " + user.getFirstName()
+          + ",\nYou are logged in as the " + user.getType();
+
+        mWelcomeTextView.setText(mWelcomeString);
+
         return v;
+
+
     }
 
 }
