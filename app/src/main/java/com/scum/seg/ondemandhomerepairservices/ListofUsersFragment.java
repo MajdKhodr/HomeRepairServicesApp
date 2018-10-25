8881ecbe10c3e620dc114423aab4e51447fa1937
Alexi10 <alexisharara@gmail.com>
2018-10-25 09:59:46 -0400

    Added fragment for the list of users

---
+++ app/src/main/java/com/scum/seg/ondemandhomerepairservices/ListofUsersFragment.java
@@ -0,0 +1,25 @@
+package com.scum.seg.ondemandhomerepairservices;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ListofUsersFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View v = inflater.inflate(R.layout.fragment_main, container, false);
+
+        return v;
+
+
+    }
+}
