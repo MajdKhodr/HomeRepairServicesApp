4853160c42480d979a362a0de7377e2de196ca7d
AliKhanafer7 <ali.h.khanafer@gmail.com>
2018-11-10 17:13:14 -0500

    added test cases for users and services

--- app/src/test/java/com/scum/seg/ondemandhomerepairservices/ExampleUnitTest.java
+++ app/src/test/java/com/scum/seg/ondemandhomerepairservices/ExampleUnitTest.java
@@ -3,6 +3,7 @@ package com.scum.seg.ondemandhomerepairservices;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +11,32 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+    User user = new User("Ali","Khanafer","akhan315","password","akhan310@gmail.com","6134136642","81 Gillespie Crescent","admin");
+    Service service = new Service("Appliance Installation", 25);
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void serviceRateIsCorrect(){
+        assertEquals("Checking service rate is correct ",25, service.getServiceRate(),0.5);
     }
+
+    @Test
+    public void serviceNameIsCorrect(){
+        assertEquals("Checking service name is correct ","Appliance Installation", service.getServiceName());
+    }
+
+    @Test
+    public void userFirstNameIsCorrect(){
+        assertEquals("Checking if the user's first name is correct ", "Ali",user.getFirstName());
+    }
+
+    @Test
+    public void userLastNameIsCorrect(){
+        assertEquals("Checking if user's last name is correct ","Khanafer",user.getLastName());
+    }
+
+    @Test
+    public void userUserNameCorrect(){
+        assertEquals("Checking if user's username is correct ","akhan315",user.getUserName());
+    }
+
 }
